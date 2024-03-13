c6d1b9c54bfb52b64f67633998c4580148804644
bnolanhussle <155196261+bnolanhussle@users.noreply.github.com>
2024-03-13 00:40:01 +0000

    Mar 13, 2024, 12:40 AM

--- script.js
+++ script.js
@@ -12,22 +12,16 @@ function addH1HeadingElement() {
  Practice 2: Adding color to loop names
  using for loop
 */
-function colorizeLoopNameHeaders() {
-
-}
+function colorizeLoopNameHeaders() {}
 
 /** 
  Practice 3: Adding grid layout to divs using
  while and do/while loops
 */
-function addFlexLayout() {
-
-}
+function addFlexLayout() {}
 
 /** 
   Practice 4:  Adding styles to articles using
   for loop
 */
-function styleArticles() {
-
-}
+function styleArticles() {}
